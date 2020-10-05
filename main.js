b41109b333c783de6d41186d29af55714b04c7a9
AvivE <aviv7385@gmail.com>
2020-10-05 17:27:54 +0300

    fixed js code

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
 // with a click on a button, display on page the location of the user (longitude and latitude) using the watchPosition() method
 //meaning - it will be updated if and when the user moves to a different location while they are moving
 
-function displayLocatioOnTheGo() {
-    setInterval(() => {
-        const location = document.getElementById("location");
-        const userLocation = navigator.geolocation.watchPosition(position => {
-            location.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude
-        })
-    }, 5000);
+function displayLocationOnTheGo() {
+    const location = document.getElementById("location");
+    navigator.geolocation.watchPosition(position => {
+        location.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude
+    });
 }
 
-displayLocatioOnTheGo()
+displayLocationOnTheGo()
